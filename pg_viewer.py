4c778249b87356d23777a00577b63692cf279be8
I-Ta Hsieh <ita.hsieh@gmail.com>
2019-05-30 13:02:13 +0800

    pg_viewer.py updated

--- pg_viewer.py
+++ pg_viewer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import psycopg2
+import pg_conf
 import numpy as np
 import struct
 
 conn = psycopg2.connect(
-    database    = "accelerometer", 
-    user        = "postgres", 
-    password    = "postgres", 
-    host        = "220.135.143.199", 
-    port        = "5432"
+    database    = pg_conf.database, 
+    user        = pg_conf.user, 
+    password    = pg_conf.password, 
+    host        = pg_conf.host,
+    port        = pg_conf.port,
     )
 print("Opened database successfully")
 
@@ -21,17 +22,19 @@ cur.execute(
     )
 dataset = cur.fetchall()
 dataset = np.array(dataset)
+print('number of data row:',len(dataset))
 
-
+TimeStamp =[]
 Array = []
 for iROW in range(len(dataset)):
     time    = dataset[iROW][1]
     length  = dataset[iROW][2]
     payload = dataset[iROW][3]
-    if len(payload) == length:
-        print('length compatable',len(payload),length)
+    if len(payload) == length == 1200:
         data = struct.unpack( 'f'*300, payload )
         Array.append(list(data))
+        TimeStamp.append(time)
+    elif len(payload) == length:
+        print('length',length,'at row',iROW+1)
     else:
         print('data loss at ',time)
-print(data)
