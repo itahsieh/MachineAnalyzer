eaa7240c72a0905285b8574c923f942d18c9a076
I-Ta Hsieh <ita.hsieh@gmail.com>
2019-09-02 13:09:28 +0800

    20190902 commitment

--- svm_fan.py
+++ svm_fan.py
@@ -12,6 +12,7 @@ FEA_type = [['Xstd',    'Ystd',     'Zstd'  ],
 
 
 lev_color = ['r','g','b']
+lev_mark = ['o','^','s']
 
 #svm = SVC(kernel = 'linear', probability = True)
 
@@ -69,28 +70,40 @@ plt.xlim(left=0,right=4)
 '''
 
 
-fig = plt.figure()
+'''
+Spec_figsize = (12, 12)
+Spec_dpi = 80
+#plt.subplots(figsize=Spec_figsize, dpi=Spec_dpi)
+
+fig = plt.figure(figsize=Spec_figsize, dpi=Spec_dpi)
 ax = fig.add_subplot(111, projection='3d')
 
-FEA_index = 2
+FEA_index = 3
 for lev_index in range(3):
     ax.scatter( balanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][0]]], 
                 balanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][1]]], 
-                balanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][2]]], 
-                marker = 'o',
-                c = lev_color[lev_index])
+                balanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][2]]],
+                label = 'Balanced, Speed Level '+str(lev_index+1),
+                marker = lev_mark[lev_index],
+                c = 'navy')
     ax.scatter( unbalanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][0]]], 
                 unbalanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][1]]], 
                 unbalanced[lev_index][:,FEA_ColumnDict[FEA_type[FEA_index][2]]], 
-                marker = '^',
-                c = lev_color[lev_index])
+                label = 'Unbalanced, Speed Level '+str(lev_index+1),
+                marker = lev_mark[lev_index],
+                c = 'darkorange')
 
 
 ax.set_xlabel(FEA_type[FEA_index][0])
 ax.set_ylabel(FEA_type[FEA_index][1])
 ax.set_zlabel(FEA_type[FEA_index][2])
+ax.legend(loc='upper right', shadow=True, fontsize='x-large')
 plt.show()
 exit()
+'''
+
+
+
 
 
 X_train = np.array([])
@@ -115,7 +128,20 @@ X_trans = pca.transform(X_train)
 #print(pca.explained_variance_)
 #print(X_trans[0,:])
 
-
+summation = 0.
+weight = np.zeros(26)
+for j in range(26):
+    weight[j] =  pca.components_[0,j]
+    weight[j] /= np.std(X_train[:,j])
+    summation += np.abs(weight[j])
+for j in range(8):
+    wx = weight[j]/summation*100. 
+    wy = weight[j+8]/summation*100.
+    wz = weight[j+16]/summation*100.
+    print('%.2f %.2f %.2f' % (wx,wy,wz))
+print('%.2f' % (weight[24]/summation*100. ))
+print('%.2f' % (weight[25]/summation*100. ))
+exit()
 
 
 
@@ -136,7 +162,6 @@ for i in range(len(target)):
 
 
 
-'''
 def SelectXY(Data, Mask, value):
     Selected = []
     for i in range(len(target)):
@@ -158,9 +183,77 @@ third_range = third_max - third_min
 balanced_data = SelectXY(X_trans[:,0:3],target,'balanced')
 unbalanced_data = SelectXY(X_trans[:,0:3],target,'unbalanced')
 
+
+
+
+
+
+
+
+'''
 Spec_figsize = (18, 12)
 Spec_dpi = 80
 plt.subplots(figsize=Spec_figsize, dpi=Spec_dpi)
+
+
+
+
+import scipy.stats as stats
+num_bins = 100
+
+plt.subplot(231)
+binwidth = (major_range+0.1*major_range) / num_bins
+bins_major = np.arange(major_min-0.05*major_range, major_max+0.05*major_range + binwidth, binwidth)
+
+dens_balanced = stats.gaussian_kde(balanced_data[:,0])
+dens_unbalanced = stats.gaussian_kde(unbalanced_data[:,0])
+
+plt.plot( bins_major, dens_balanced(bins_major))
+plt.plot( bins_major, dens_unbalanced(bins_major))
+
+plt.fill(bins_major, dens_balanced(bins_major), 'navy',
+         bins_major, dens_unbalanced(bins_major), 'darkorange', alpha=0.3)
+
+plt.xlim( major_min - 0.1 * major_range, major_max  + 0.1 * major_range)
+plt.ylim( bottom = 1e-5)
+plt.yscale("log")
+
+
+plt.subplot(232)
+binwidth = (second_range+0.1*second_range) / num_bins
+bins_second = np.arange(second_min-0.05*second_range, second_max+0.05*second_range + binwidth, binwidth)
+
+dens_balanced = stats.gaussian_kde(balanced_data[:,1])
+dens_unbalanced = stats.gaussian_kde(unbalanced_data[:,1])
+
+plt.plot( bins_second, dens_balanced(bins_second))
+plt.plot( bins_second, dens_unbalanced(bins_second))
+
+plt.fill(bins_second, dens_balanced(bins_second), 'navy',
+         bins_second, dens_unbalanced(bins_second), 'darkorange', alpha=0.3)
+
+plt.xlim( second_min - 0.1 * second_range, second_max  + 0.1 * second_range)
+plt.ylim( bottom = 1e-5)
+plt.yscale("log")
+
+
+plt.subplot(233)
+binwidth = (third_range+0.1*third_range) / num_bins
+bins_third = np.arange(third_min-0.05*third_range, third_max+0.05*third_range + binwidth, binwidth)
+
+dens_balanced = stats.gaussian_kde(balanced_data[:,2])
+dens_unbalanced = stats.gaussian_kde(unbalanced_data[:,2])
+
+plt.plot( bins_third, dens_balanced(bins_third))
+plt.plot( bins_third, dens_unbalanced(bins_third))
+
+plt.fill(bins_third, dens_balanced(bins_third), 'navy',
+         bins_third, dens_unbalanced(bins_third), 'darkorange', alpha=0.3)
+
+plt.xlim( third_min - 0.1 * third_range, third_max  + 0.1 * third_range)
+plt.ylim( bottom = 1e-5)
+plt.yscale("log")
+
 
 plt.subplot(234)
 
@@ -192,11 +285,14 @@ plt.scatter(unbalanced_data[:,2], unbalanced_data[:,0] , c='darkorange', s=0.5, 
 plt.xlabel('Third Component')
 plt.ylabel('Major Component')
 
-plt.xlim( third_min - 0.1 * third_range, third_max  + 0.1 * third_range)
-plt.ylim( major_min- 0.1 *major_range, major_max + 0.1 * major_range)
-
-plt.show()
+plt.xlim( third_min - 0.1 * third_range, third_max + 0.1 * third_range)
+plt.ylim( major_min - 0.1 * major_range, major_max + 0.1 * major_range)
 '''
+
+
+
+
+
 
 
 
@@ -261,33 +357,64 @@ print('False Negative:',FN/false)
 '''
 
 
-
-
-
-
-
-
-
-
 '''
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Spec_figsize = (12, 12)
+Spec_dpi = 80
+#plt.subplots(figsize=Spec_figsize, dpi=Spec_dpi)
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+fig = plt.figure(figsize=Spec_figsize, dpi=Spec_dpi)
+ax = fig.add_subplot(111, projection='3d')
 
-# Plot also the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.xticks(())
-plt.yticks(())
-plt.title(titles[i])
+ax.scatter( balanced_data[:,0],
+            balanced_data[:,1],
+            balanced_data[:,2],
+            label = 'Balanced',
+            marker = 'o',
+            c = 'navy')
+ax.scatter( unbalanced_data[:,0],
+            unbalanced_data[:,1],
+            unbalanced_data[:,2],
+            label = 'Unbalanced',
+            marker = 'o',
+            c = 'darkorange')
+
+
+ax.scatter( clf.support_vectors_[:,0],
+            clf.support_vectors_[:,1],
+            clf.support_vectors_[:,2],
+            label = 'Sopport Vectors',
+            marker = '^',
+            s = 100,
+            c = 'green')
+
+
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+xx = np.linspace(-1100, -200, 30)
+yy = np.linspace(ylim[0], ylim[1], 30)
+YY, XX = np.meshgrid(yy, xx)
+xy = np.vstack([XX.ravel(), YY.ravel()]).T
+
+Z = np.zeros(xy.shape[0])
+#print(clf.intercept_);exit()
+for i in range(Z.shape[0]):
+    Z[i] = (-clf.intercept_[0] - clf.coef_[0,0] * xy[i,0] - clf.coef_[0,1] * xy[i,1]) / clf.coef_[0,2] 
+Z = Z.reshape(XX.shape)
+#print(Z);exit()
+ax.plot_surface(XX,YY,Z,alpha=0.2,color='red')
+
+ax.set_xlabel('Major Component')
+ax.set_ylabel('Second Component')
+ax.set_zlabel('third Component')
+ax.legend(loc='upper right', shadow=True, fontsize='x-large')
 
 plt.show()
+
 '''
+
+
+
+
 
 
 
